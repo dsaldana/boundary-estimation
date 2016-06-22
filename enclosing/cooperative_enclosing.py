479e54912cac572616bc6571b500f418e451996a
david <dajulian@gmail.com>
2016-06-22 02:03:44 -0400

    zero is working

--- enclosing/cooperative_enclosing.py
+++ enclosing/cooperative_enclosing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 ### Generate
 from enclosing.Agent import Agent
-from enclosing.s_estimator import cut_polyline, get_perpendicular
+from enclosing.s_estimator import cut_polyline, get_perpendicular, update_zero
 
 
 def boundaries_on_time(t_steps=500):
@@ -28,6 +28,12 @@ def boundaries_on_time(t_steps=500):
     return np.array(boundaries)
 
 
+# Draw
+draw_perps = False  # Perpendicular lines
+draw_paths = False
+draw_init_polyset = False
+draw_polysets = True
+
 #######################################
 # Compute boundaries
 boundaries = boundaries_on_time()
@@ -44,8 +50,7 @@ M = len(boundary0)
 iloc = [7 * M / 10, M / 2, M / 10]
 agents = [Agent(boundary1[i - 1], boundary0[i]) for i in iloc]
 
-
-####### Initial path
+####### Initial path ############
 initial_steps = 80
 # move the robots with constant velocity
 for boundary in boundaries[2:initial_steps]:
@@ -54,12 +59,13 @@ for boundary in boundaries[2:initial_steps]:
         a.move_on_boundary(boundary, vel)
 
 # Draw initial path
-for a in agents:
-    plt.plot(a.traj_x[-1], a.traj_y[-1], 'o')
-    plt.plot(a.traj_x, a.traj_y, '.')
+if draw_paths:
+    for a in agents:
+        plt.plot(a.traj_x[-1], a.traj_y[-1], 'o')
+        plt.plot(a.traj_x, a.traj_y, '-')
 
-bx, by = boundaries[initial_steps - 1].T
-plt.plot(bx, by, '--')
+    bx, by = boundaries[initial_steps - 1].T
+    plt.plot(bx, by, '--')
 
 # plt.show()
 # plt.ion()
@@ -67,7 +73,7 @@ plt.plot(bx, by, '--')
 
 
 
-######## Initial paths ################
+######## Initial multi-line-string ################
 # polyline for robot 0
 polyset = [(agents[0].traj_x, agents[0].traj_y)]
 
@@ -77,13 +83,6 @@ for i in range(1, N + 1):
     # Trajectory for robot i
     tx, ty = a.traj_x, a.traj_y
 
-    # perpendicular line
-
-    # plt.clf()
-    # plt.plot([pline[0][0], pline[1][0]], [pline[0][1], pline[1][1]])
-    # plt.plot(polyset[i-1][0], polyset[i-1][1], 'o')
-    # plt.show()
-    print i
     # Remove after the cut
     polyset[i - 1] = cut_polyline((tx, ty), polyset[i - 1])
 
@@ -96,23 +95,42 @@ for i in range(1, N + 1):
     # plt.show()
 
     ### Draw
-    (p1, p2) = get_perpendicular(tx, ty)
-    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], '-')
-    plt.xlim([-1.5, 2])
-    plt.ylim([-1.5, 1.5])
+    if draw_perps:
+        (p1, p2) = get_perpendicular(tx, ty)
+        plt.plot([p1[0], p2[0]], [p1[1], p2[1]], '-')
+        plt.xlim([-1.5, 2])
+        plt.ylim([-1.5, 1.5])
 
-for (polyx, polyy) in polyset:
-    plt.plot(polyx, polyy, 'b')
+## Zero point
+ztx, zty = polyset[0][0], polyset[0][1]
+idz = (0, len(ztx) - 1)  # polyline, id point in polyline
+zero = ztx[idz[1]], zty[idz[1]]
+(zp1, zp2) = get_perpendicular(ztx, zty)
+
+# draw
+if draw_init_polyset:
+    for (polyx, polyy) in polyset:
+        plt.plot(polyx, polyy, 'b')
+        plt.plot(zero[0], zero[1], 'x')
+        # Zero perpendicular
+        plt.plot([zp1[0], zp2[0]], [zp1[1], zp2[1]], '-')
+        plt.xlim([-1.5, 2])
+        plt.ylim([-1.5, 1.5])
 plt.show()
 
+
 ###############
-# Parametrize the polyline
-############### Update
+# Update multi-line string
+###############
 
-for k in range(30):
+
+
+for k in range(500):
+    print k
     # for a in agents:
-    boundary = boundaries[initial_steps+k]
+    boundary = boundaries[initial_steps + k]
 
+    # For each agent
     for i in range(N):
         a = agents[i]
         a.move_on_boundary(boundary, .1)
@@ -130,9 +148,20 @@ for k in range(30):
         # Remove after the cut
         polyset[i - 1] = cut_polyline((a.traj_x, a.traj_y), polyset[i - 1])
 
-    for (polyx, polyy) in polyset:
-        plt.plot(polyx, polyy, 'b')
-    plt.show()
+        # Update zero perpendicular
+        zero, id_c, (zp1, zp2), polyset = update_zero(zero, (zp1, zp2), polyset)
+        print id_c
+
+    if draw_polysets :
+        for (polyx, polyy) in polyset:
+            plt.plot(polyx, polyy, 'b.-')
+            # Zero
+            plt.plot(zero[0], zero[1], 'x')
+            # Zero perpendicular
+            plt.plot([zp1[0], zp2[0]], [zp1[1], zp2[1]], '-')
+            plt.xlim([-1.5, 2])
+            plt.ylim([-1.5, 1.5])
+        plt.show()
 
 
 
